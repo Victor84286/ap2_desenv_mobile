2d16e18f28086cc5234007d2c2ea7e2e9de5c43c
Victor Hugo <victorhrapinto@gmail.com>
2023-06-13 14:43:10 -0300

    envio trabalho final

--- Ap2DevMobile/app/src/main/java/com/example/ap2devmobile/MainActivity.kt
+++ Ap2DevMobile/app/src/main/java/com/example/ap2devmobile/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.ap2devmobile
 
+import android.app.SearchManager
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +16,14 @@ class MainActivity : AppCompatActivity() {
         botao.setOnClickListener {
             val intent = Intent(this, Questao1::class.java)
             startActivity(intent)
+        }
+
+        val botaoGithub: ImageButton = findViewById(R.id.btnImage)
+        botaoGithub.setOnClickListener {
+            val query = "https://br.linkedin.com/in/victor-hugo-rocha/en?trk=people-guest_people_search-card"
+            val searchIntent = Intent(Intent.ACTION_WEB_SEARCH)
+            searchIntent.putExtra(SearchManager.QUERY, query)
+            startActivity(searchIntent)
         }
     }
 
